825016c2709bb1347e41827808052463c171b393
santhosh <santhosh20sekar@gmail.com>
2024-12-12 21:45:17 +0530

    [santhosh] added: customer creation service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,44 +1,19 @@
-import User from '../models/user.model';
-import { IUser } from '../interfaces/user.interface';
+import customer from '../models/customer.model';
+import { Customer } from '../interfaces/customer.interface';
 
 class UserService {
-
-  //get all users
-  public getAllUsers = async (): Promise<IUser[]> => {
-    const data = await User.find();
-    return data;
-  };
-
-  //create new user
-  public newUser = async (body: IUser): Promise<IUser> => {
-    const data = await User.create(body);
-    return data;
-  };
-
-  //update a user
-  public updateUser = async (_id: string, body: IUser): Promise<IUser> => {
-    const data = await User.findByIdAndUpdate(
-      {
-        _id
-      },
-      body,
-      {
-        new: true
+  public createCustomer = async (body: Customer): Promise<Customer> => {
+    try {
+      const existingCustomer = await customer.findOne({ email: body.email });
+      if (existingCustomer) {
+        throw new Error('Customer with this email already exists.');
       }
-    );
-    return data;
-  };
+      const newCustomer = await customer.create(body);
+      return newCustomer;
 
-  //delete a user
-  public deleteUser = async (_id: string): Promise<string> => {
-    await User.findByIdAndDelete(_id);
-    return '';
-  };
-
-  //get a single user
-  public getUser = async (_id: string): Promise<IUser> => {
-    const data = await User.findById(_id);
-    return data;
+    } catch (error) {
+      throw new Error(`Error creating customer: ${error.message}`);
+    }
   };
 }
 
