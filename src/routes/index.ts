3b4492ec423a205e4ca8e6f31119ece6a8fa0707
Joshua K John <joshuakjohn7@gmail.com>
2024-12-13 16:55:50 +0530

    [joshua] feat : added validation for agent registration details and removed users api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import express, { IRouter } from 'express';
 const router = express.Router();
 
-import userRoute from './user.route';
+import agentRoute from './agent.route';
 
 /**
  * Function contains Application routes
@@ -12,7 +12,7 @@ const routes = (): IRouter => {
   router.get('/', (req, res) => {
     res.json('Welcome');
   });
-  router.use('/users', new userRoute().getRoutes());
+  router.use('/agent', new agentRoute().getRoutes());
 
   return router;
 };
---
+++ src/validators/agent.validator.ts
@@ -0,0 +1,24 @@
+import Joi from "@hapi/joi";
+import { NextFunction } from "express";
+
+
+class AgentValidator{
+
+    public newAgent = (req: Request, res: Response, next: NextFunction): void => {
+        const schema = Joi.object({
+          name: Joi.string().required(),
+          email: Joi.string().email().required(),
+          password: Joi.string().min(4).required(),
+          phone: Joi.string().pattern(/^[0-9]{10}$/).required(),
+          region: Joi.string().required()
+        });
+        const { error } = schema.validate(req.body);
+        if (error) {
+          next(error);
+        }
+        next();
+      };
+
+}
+
+export default AgentValidator
