f87d48907bd76f77d66e386d6a7f4e3b67c322f3
Joshua K John <joshuakjohn7@gmail.com>
2024-12-19 16:10:32 +0530

    [joshua] feat: added authorization to customer routes

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -1,7 +1,7 @@
 import express, { IRouter } from 'express';
 import CustomerController from '../controllers/customer.controller';
 import CustomerValidator from '../validators/customer.validator'; 
-import { agentAuth, customerAuth } from '../middlewares/auth.middleware';
+import { adminAuth, agentAuth, customerAuth } from '../middlewares/auth.middleware';
 
 class UserRoutes {
   private CustomerController = new CustomerController();
@@ -13,46 +13,31 @@ class UserRoutes {
   }
 
   private routes = () => {
-    //route to register a customer
-    this.router.post(
-      '/register',
-      this.CustomerValidator.createCustomer, 
-      this.CustomerController.createCustomer 
-    );
-
-    //route to login a customer
-    this.router.post(
-      '',
-      this.CustomerValidator.customerLogin,
-      this.CustomerController.customerLogin
-    ); 
-   
-   //route to get all customer
-   this.router.get(
-    '/',
-    agentAuth,
-    this.CustomerController.getAllCustomers
-    ); 
     
-    this.router.get('/refreshtoken/:id',this.CustomerController.refreshToken)
+    //route to login a customer
+    this.router.post('', this.CustomerValidator.customerLogin, this.CustomerController.customerLogin); 
 
-    this.router.post('/paypremium',this.CustomerController.payPremium )
+    //route to get all customer, by agent
+    this.router.get('', agentAuth, this.CustomerController.getAllCustomers);
+
+    //route to register a customer
+    this.router.post( '/register', this.CustomerValidator.createCustomer, this.CustomerController.createCustomer);
+    
+    //route to get all customer, by admin
+    this.router.get('/admin', adminAuth, this.CustomerController.getAllCustomers);
+
+    //route for customer to pay premium
+    this.router.post('/paypremium', customerAuth, this.CustomerController.payPremium )
 
     // forget password route
-    this.router.post(
-      '/forgot-password',
-      this.CustomerValidator.validateForgotPassword,
-      this.CustomerController.forgotPassword
-    );
+    this.router.post('/forgot-password', customerAuth, this.CustomerValidator.validateForgotPassword, this.CustomerController.forgotPassword);
     
-    // Reset Password
-    this.router.post(
-      '/reset-password', 
-      this.CustomerValidator.validateResetPassword, 
-      customerAuth, 
-      this.CustomerController.resetPassword
-    );
+    // Reset Password route
+    this.router.post('/reset-password', this.CustomerValidator.validateResetPassword, customerAuth, this.CustomerController.resetPassword);
     
+    //route to refresh token
+    this.router.get('/:id/refreshtoken', this.CustomerController.refreshToken)
+
   };
 
   public getRoutes = (): IRouter => {
