a5f385089e21ffbe5db836addb025d637f83e515
Joshua K John <joshuakjohn7@gmail.com>
2024-12-20 12:38:45 +0530

    [joshua] feat: added employee controller

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -1,6 +1,21 @@
 
 
 class EmployeeService{
+    refreshToken(employeeId: string) {
+        throw new Error("Method not implemented.");
+    }
+    resetPassword(body: any, adminId: any) {
+        throw new Error("Method not implemented.");
+    }
+    forgotPassword(email: any) {
+        throw new Error("Method not implemented.");
+    }
+    createEmployee(body: any) {
+        throw new Error("Method not implemented.");
+    }
+    loginEmployee(body: any): { token: any; refreshToken: any; username: any; email: any; } | PromiseLike<{ token: any; refreshToken: any; username: any; email: any; }> {
+        throw new Error("Method not implemented.");
+    }
 
 }
 
