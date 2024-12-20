664d77e211db96eb5cb679809d20f6203b9ff866
Anjali Gour <gour26000@gmail.com>
2024-12-20 12:09:06 +0530

    [anjali] feat : implemented pagination for get all schemes by customer and agent

--- src/routes/scheme.route.ts
+++ src/routes/scheme.route.ts
@@ -20,10 +20,10 @@ class SchemeRoutes {
          this.router.get('/', adminAuth, this.schemeValidator.validatePagination, this.schemeController.getAllSchemes);
 
          //route to get all schemes by customer
-         this.router.get('/customer', customerAuth, this.schemeController.getAllSchemes);
+         this.router.get('/customer', customerAuth, this.schemeValidator.validatePagination, this.schemeController.getAllSchemes);
 
          //route to get all schemes by agent
-         this.router.get('/agent', agentAuth, this.schemeController.getAllSchemes);
+         this.router.get('/agent', agentAuth, this.schemeValidator.validatePagination, this.schemeController.getAllSchemes);
 
          //route to get a scheme by id, by admin
          this.router.get('/:id', adminAuth, this.schemeController.getSchemeById);
