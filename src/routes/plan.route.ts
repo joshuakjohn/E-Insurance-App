22368304e3531ce4d590fe7f0fb3f8dbab6331c0
Joshua K John <joshuakjohn7@gmail.com>
2024-12-24 11:13:32 +0530

    [joshua] fix: removed unused routes from plan and scheme

--- src/routes/plan.route.ts
+++ src/routes/plan.route.ts
@@ -18,35 +18,17 @@ class PlanRoutes {
     // create plan route by admin
     this.router.post('/', adminAuth, this.planValidator.createPlan, this.planController.createPlan);
 
-    // get all plans by admin
-    this.router.get('/', adminAuth, this.planValidator.validatePagination, cacheData, this.planController.getAllPlans);
+    // get all plans
+    this.router.get('/', this.planValidator.validatePagination, cacheData, this.planController.getAllPlans);
 
-    // get all plans by customer
-    this.router.get('/customer', customerAuth, this.planValidator.validatePagination, cacheData, this.planController.getAllPlans);
-
-    // get all plans by agent
-    this.router.get('/agent', agentAuth, this.planValidator.validatePagination, cacheData, this.planController.getAllPlans);
-
-    // get all plans by employee
-    this.router.get('/employee', employeeAuth, this.planValidator.validatePagination, cacheData, this.planController.getAllPlans);
-
-    // get plan by id, by admin
-    this.router.get('/:id', adminAuth, this.planController.getPlanById);
+    // get plan by id
+    this.router.get('/:id', this.planController.getPlanById);
 
     // update a plan by id, by admin
     this.router.put('/:id', adminAuth, this.planValidator.updatePlan, this.planController.updatePlan);
 
     // delete a plan by id, by admin
     this.router.delete('/:id', adminAuth, this.planController.deletePlan);
-
-    // get plan by id, by customer
-    this.router.get('/:id/customer', customerAuth, this.planController.getPlanById);
-
-    // get plan by id, by agent
-    this.router.get('/:id/agent', agentAuth, this.planController.getPlanById);
-
-    // get plan by id, by employee
-    this.router.get('/:id/employee', employeeAuth, this.planController.getPlanById);
 
   };
 
